978772f3b77a131f7dac7aff012113f5fc3b5ac4
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-05-13 11:50:52 -0300

    Cores para o Terminal

---
+++ cores.h
@@ -0,0 +1,15 @@
+#ifndef CORES_H
+#define CORES_H
+
+/*
+Aqui será apenas as cores que usarei no terminal.
+*/
+// Cores para o terminal:
+#define RESET    "\033[0m"
+#define BRANCO   "\033[97m"
+#define AZUL     "\033[94m"
+#define VERMELHO "\033[91m"
+#define VERDE    "\033[92m"
+#define CINZA    "\033[90m"
+
+#endif
