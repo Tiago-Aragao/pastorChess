172dbc69bd3cd2582f57230f53d86520ebd7a59e
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-05-13 13:37:21 -0300

    Criação das bibliotecas e protótipos das funções

---
+++ funcoes_sistema.h
@@ -0,0 +1,11 @@
+#ifndef FUNCOES_SISTEMA_H
+#define FUNCOES_SISTEMA_H
+
+// Head_master:
+#include "head_master.h"
+
+// Protótipos das funções:
+void limparMatriz(ENTIDADE matriz[LINHA][COLUNA]);
+void gerarEntidades (ENTIDADE matriz[LINHA][COLUNA], TIPO_ENTIDADE ent, int quant_ent);
+void exibirMatriz (ENTIDADE matriz[LINHA][COLUNA]);
+#endif
---
+++ funcoes_uteis.h
@@ -0,0 +1,8 @@
+#ifndef FUNCOES_UTEIS_H
+#define FUNCOES_UTEIS_H
+
+
+// Protótipos das funções uteis:
+void limparTela();
+
+#endif
