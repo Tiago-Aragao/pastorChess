43a933b1cca7c1b7ad61af3af6f5cdc8743f9edb
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-05-13 14:35:55 -0300

    Adição: Função que exibe a matriz

--- funcoes_sistema.c
+++ funcoes_sistema.c
@@ -1,6 +1,10 @@
 #include "head_master.h"
+#include "funcoes_uteis.h"
+#include "cores.h"
 
+// Função para limpar a matriz antes de trabalhar:
 void limparMatriz(ENTIDADE matriz[LINHA][COLUNA]){
+    
     /*
     Aqui eu vou limpar a minha matriz que está cheia de lixo de memória, dessa forma "preenchendo"
     ela de espaços vazios.
@@ -16,7 +20,17 @@ void limparMatriz(ENTIDADE matriz[LINHA][COLUNA]){
     }
 }
 
+// Gerar Entidades (Pastor, Lobo, Ovelha e Obstáculo):
 void gerarEntidades (ENTIDADE matriz[LINHA][COLUNA], TIPO_ENTIDADE ent, int quant_ent) {
+
+    /*
+    Está função gera as entidades para popular a matriz por metodo de sorteio de casas.
+    Entidades a serem geradas:
+    >>> Pastor
+    >>> Lobo
+    >>> Ovelha
+    >>> Obstáculo
+    */
 
     // Declaro e inicializo quantos obstaculos tem atualmente:
     int cont_ent = 0;
@@ -36,4 +50,45 @@ void gerarEntidades (ENTIDADE matriz[LINHA][COLUNA], TIPO_ENTIDADE ent, int quan
             cont_ent++; // incremento o contador
         }
     } // Fim do Loop.
+}
+
+void exibirMatriz (ENTIDADE matriz[LINHA][COLUNA]) {
+    
+    // Limpo a tela antes de cada rodada:
+    limparTela();
+
+    // Separador:
+    printf("---------------------------\n");
+    
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            
+            // O switch avalia o qual o tipo e pinta a letra correspondente:
+            switch (matriz[i][j].tipo) {
+                case VAZIO:
+                    // Um ponto verde para representar a grama:
+                    printf(VERDE ". " RESET); 
+                    break;
+                case OBSTACULO:
+                    // Uma cerquilha cinza para os obstáculos:
+                    printf(CINZA "# " RESET); 
+                    break;
+                    // Branco para ovvelhas:
+                case OVELHA:
+                    printf(BRANCO "O " RESET);
+                    break;
+                    // Vermelho para os lobos:
+                case LOBO:
+                    printf(VERMELHO "L " RESET);
+                    break;
+                    // Azul para os pastores:
+                case PASTOR:
+                    printf(AZUL "P " RESET);
+                    break;
+            }
+        }
+        printf("\n"); // Quebra a linha toda vez que terminar de imprimir as colunas
+    }
+    
+    printf("---------------------------\n");
 }
