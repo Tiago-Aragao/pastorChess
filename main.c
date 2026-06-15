385e030acabd6c4cc7fe0f8dcfa3c8c8285b6408
Tiago-Aragao <tiagoaragao010398@gmail.com>
2026-06-15 11:17:25 -0300

    Atualização para adaptar as novas funções implementadas.

--- main.c
+++ main.c
@@ -6,63 +6,66 @@ int main () {
 
     // Para garantir uma nova Seed a cada interação uso o srand:
     srand(time(NULL));
-    // Chamo a configuração do Terminal:
+    // Configurando o terminal para ANSI:
     configurarTerminal();
 
     // Definindo as Matriz:
-    ENTIDADE matriz_atual[LINHA][COLUNA]; // OBS: read only
-    ENTIDADE matriz_futura[LINHA][COLUNA]; // OBS: Matriz que guarda o futuro
+    // Agora eu inicializo elas já limpas em vez de rodar um laço for para limpar as matrizes.
+    ENTIDADE matriz_atual[LINHA][COLUNA] = {0}; // OBS: read only
+    ENTIDADE matriz_futura[LINHA][COLUNA] = {0}; // OBS: Matriz que guarda o futuro
     // Declarando e sorteando a quantidade de obstáculos:
     int qtd_obs = (rand() % FAIXA) + MIN_OBSTACULOS;
     // Declarando variaveis gerais:
     int rodadas = 0, rodando = 1, pontuacao_lobos = 0, pontuacao_pastores = 0;
 
+    // Inicialmente no MVP remove a necessidade de ter um laço for apenas para limpar as matrizes:
     // Limpando a matriz:
-    limparMatriz(matriz_atual);
-    limparMatriz(matriz_futura);
+    // limparMatriz(matriz_atual);
+    // limparMatriz(matriz_futura);
     
     // Gerando as entidades (quantidades para teste):
     gerarEntidades(matriz_atual, OBSTACULO, qtd_obs);
-    gerarEntidades(matriz_atual, OVELHA, 45);
-    gerarEntidades(matriz_atual, LOBO, 2);
-    gerarEntidades(matriz_atual, PASTOR, 2);
+    gerarEntidades(matriz_atual, OVELHA, 50);
+    gerarEntidades(matriz_atual, LOBO, 5);
+    gerarEntidades(matriz_atual, PASTOR, 5);
 
+    // Limpo o terminal antes de iniciar o loop:
+    system("cls");
+    
     while(1){
         // Exibindo a matriz:
         exibirMatriz(matriz_atual);
         printf("-------------------------------------\n");
-        printf("Matriz atual -- TURNOS: %d \n", rodadas);
+        printf("Matriz atual -- RODADAS: %d \n", rodadas);
         printf("Numero de Obstaculos: %d \n", qtd_obs);
         printf("-------------------------------------\n");
         printf("               PONTUACAO\n");
         printf("-------------------------------------\n");
         printf("PASTORES: %d\n", pontuacao_pastores);
         printf("LOBOS: %d\n", pontuacao_lobos);
+
         // Pausa para dar um sweet move para os membros da matriz:
-        Sleep(250);
-        
+        Sleep(200);
+            
         //TURNO 1: OVELHA
         // Copio a matriz atual para a futura:
         memcpy(matriz_futura, matriz_atual, sizeof(matriz_atual));
-        // Movo a ovelha:
-        moverOvelha(matriz_atual, matriz_futura);
+        // Disparo o motor para processar todas as ovelhas:
+        processarTurno(OVELHA, matriz_atual, matriz_futura);
         // Atualizo a matriz atual com as infos da matriz futura:
         memcpy(matriz_atual, matriz_futura, sizeof(matriz_futura));
 
         //TURNO 2: LOBOS
-        // Copio a matriz atual para a futura:
         memcpy(matriz_futura, matriz_atual, sizeof(matriz_atual));
-        // Movo o lobo:
-        pontuacao_lobos += moverLobo(matriz_atual, matriz_futura);
-        // Atualizo a matriz atual com as infos da matriz futura:
+        // Disparo o motor para processar todos os lobos e guardo os pontos:
+        pontuacao_lobos += processarTurno(LOBO, matriz_atual, matriz_futura);
         memcpy(matriz_atual, matriz_futura, sizeof(matriz_futura));
 
         //TURNO 3: PASTOR
-        // Copio a matriz atual para a futura:
         memcpy(matriz_futura, matriz_atual, sizeof(matriz_atual));
-        // Movo o pastor:
-        pontuacao_pastores += moverPastor(matriz_atual, matriz_futura);
-        // Atualizo a matriz atual com as infos da matriz futura:
+        // Disparo o motor para o pastor. 
+        // A Tabela vai automaticamente rodar a lógica do lobo para ele!
+        pontuacao_pastores += processarTurno(PASTOR, matriz_atual, matriz_futura);
         memcpy(matriz_atual, matriz_futura, sizeof(matriz_futura));
 
         rodadas++;
